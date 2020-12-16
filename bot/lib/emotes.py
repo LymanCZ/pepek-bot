f94afcb363a4e79244a263bf3ea6355516ad847f
Daniel <daniel@ideapad.lan>
2020-12-16 17:48:09 +0100

    Moved remaining emotes

--- bot/lib/emotes.py
+++ bot/lib/emotes.py
@@ -18,3 +18,22 @@ scoots_emoji = [
     "<:forsenScoots:736973346142552195>",
     "<:OMGScoots:736973384570634321>"
 ]
+
+dance_emoji = [
+    "<a:forsenPls:741611256460476496>",
+    "<a:forsenDiscoSnake:742013168234135664>",
+    "<a:headBang:742013167890333802>",
+    "<a:KKool:742013168196517899>" + " <a:GuitarTime:742013167554789390>",
+    "<a:pepeJAM:742013167671967805>",
+    "<a:AlienPls:742014131305054239>",
+    "<a:SHUNGITE:744474032698556477> " + "<a:doctorDance:744473646298431510>" + " <a:SHUNGITE:744474032698556477>"
+]
+dance_react = [
+    "<a:forsenPls:741611256460476496>",
+    "<a:forsenDiscoSnake:742013168234135664>",
+    "<a:headBang:742013167890333802>",
+    "<a:KKool:742013168196517899>",
+    "<a:pepeJAM:742013167671967805>",
+    "<a:AlienPls:742014131305054239>",
+    "<a:doctorDance:744473646298431510>"
+]
