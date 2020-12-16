baa6d54b4cd536b45d0807b11c60d615edee4fb7
Daniel <daniel@ideapad.lan>
2020-12-16 11:49:56 +0100

    Moved global emotes to library

---
+++ bot/lib/emotes.py
@@ -0,0 +1,15 @@
+basic_emoji = {
+    # "Si": "<:Si:523966164704034837>",
+    "Si": "<:forsenT:743128058545832048>",
+    "Sadge": "<:Sadge:696437945392955453>",
+    "Pepega": "<:Pepega:739020602194657330>",
+    "forsenSmug": "<:forsenSmug:736973361283858442>",
+    "forsenScoots": "<:forsenScoots:736973346142552195>",
+    "forsenT": "<:forsenT:743128058545832048>",
+    "docSpin": "<a:docSpin:743133871889055774>",
+    "hackerCD": "<a:hackerCD:744835324827402250>",
+    "Clap": "<a:Clap:746628991753912401>",
+    "FeelsWeirdMan": "<:FeelsWeirdMan:750942213676073012>",
+    "Okayga": "<:Okayga:750974019104276521>",
+    "residentCD": "<:residentCD:750974439092518922>"
+}
