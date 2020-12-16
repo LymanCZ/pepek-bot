8b5f0128f20126bcfcbddd2428b357fa9f297c74
Daniel <daniel@ideapad.lan>
2020-12-16 17:48:38 +0100

    Moved the rest of global variables

--- bot/lib/config.py
+++ bot/lib/config.py
@@ -1,3 +1,6 @@
+import os
+
+
 # HTTP headers
 headers = {
     "Access-Control-Allow-Origin": "*",
@@ -5,4 +8,34 @@ headers = {
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Max-Age": "3600",
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
+}
+
+# Log-in for youtube to download age-restricted videos ~this still doesn't solve the issue, I don't understand why~
+YT_MAIL = os.getenv("YT_MAIL")
+YT_PASS = os.getenv("YT_PASS")
+# youtube-dl wants cookies as a text file ~this also doesn't solve the age-restriction issue~
+with open("cookies.txt", "w") as text_file:
+    print(os.getenv('COOKIE_DATA'), file=text_file)
+
+ytdl_format_options = {
+    "cookies": "cookies.txt",
+    "user_agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "username": YT_MAIL,
+    "password": YT_PASS,
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0"  # Bind to IPv4 since IPv6 addresses cause issues sometimes
+}
+
+# -vn discards video stream
+ffmpeg_options = {
+    "options": "-vn"
 }
